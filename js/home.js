71f4cacd30b7db2a8abf7381b0066fcfd83f3a99
Ahmed Rabea Smaha <35260162+ahmedrabeasmaha@users.noreply.github.com>
2024-11-29 21:31:48 +0200

    first commit

--- js/home.js
+++ js/home.js
@@ -1,6 +1,8 @@
 var currentUser;
 var welcome = document.querySelector("#welcome");
 var logOut = document.querySelector("#logOut");
+console.log(location);
+console.log(location.hostname);
 if (localStorage.getItem("currentUser")) {
   currentUser = localStorage.getItem("currentUser");
   welcome.innerHTML(`Hello ${currentUser}`);
