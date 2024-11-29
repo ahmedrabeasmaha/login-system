f1e37b5adf570a397de30e8a04c14e808444f4d6
Ahmed Rabea Smaha <35260162+ahmedrabeasmaha@users.noreply.github.com>
2024-11-29 22:11:42 +0200

    first commit

--- js/home.js
+++ js/home.js
@@ -4,7 +4,7 @@ var logOut = document.querySelector("#logOut");
 
 if (localStorage.getItem("currentUser")) {
   currentUser = localStorage.getItem("currentUser");
-  welcome.innerHTML(`Hello ${currentUser}`);
+  welcome.innerHTML = `Hello ${currentUser}`;
 } else {
   location.replace(`https://${location.hostname}/login-system/signin.html`);
 }
